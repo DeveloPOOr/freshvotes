eb29982528dcf6856c37b8d48903597a6292437e
danilcikilev <bzdanya@gmail.com>
2020-07-17 17:22:20 +0500

    Initial commit

---
+++ src/main/java/com/FreshvotesApplication.java
@@ -0,0 +1,13 @@
+package com.freshvotes.freshvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshvotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshvotesApplication.class, args);
+	}
+
+}
