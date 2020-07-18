4767588086c136fdd459b732fd730ca99e551cdd
danilcikilev <bzdanya@gmail.com>
2020-07-18 16:15:23 +0500

    Revert "entities"
    This reverts commit 9a708b674a17cf6a55db78afceadcb726f89afb0.

--- src/main/java/com/FreshvotesApplication.java
+++ src/main/java/com/FreshvotesApplication.java
@@ -1,13 +1,13 @@
-package com.freshvotes;
+package com.freshvotes.freshvotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FreshVotesApplication {
+public class FreshvotesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FreshVotesApplication.class, args);
+		SpringApplication.run(FreshvotesApplication.class, args);
 	}
 
 }
