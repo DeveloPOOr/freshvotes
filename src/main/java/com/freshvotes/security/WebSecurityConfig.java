b1b604d4c95ec496284c91097b894c0eb87f5970
danilcikilev <bzdanya@gmail.com>
2020-07-26 13:36:11 +0500

    k

--- src/main/java/com/freshvotes/security/WebSecurityConfig.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-         auth.inMemoryAuthentication()    //если прервать сервер все сотрется
+         auth.inMemoryAuthentication()//если прервать сервер все сотрется
+                 .passwordEncoder(getPasswordEncoder())
                  .withUser("Debil")
-                 .password("123")
+                 .password(getPasswordEncoder().encode("123"))
                  .roles("USER");
     }
 
