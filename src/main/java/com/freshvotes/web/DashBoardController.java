9870de462f0fcb9d6725eab47f184606bc4beeec
danilcikilev <bzdanya@gmail.com>
2020-07-26 22:38:32 +0500

    csrf protection

--- src/main/java/com/freshvotes/web/DashBoardController.java
+++ src/main/java/com/freshvotes/web/DashBoardController.java
@@ -2,6 +2,7 @@ package com.freshvotes.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
