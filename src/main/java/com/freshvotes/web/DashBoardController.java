dac7467bf48ced1cd8f354d0ab8ece247618d5ad
danilcikilev <bzdanya@gmail.com>
2020-07-26 13:56:56 +0500

    dashborad

--- src/main/java/com/freshvotes/web/DashBoardController.java
+++ src/main/java/com/freshvotes/web/DashBoardController.java
@@ -1,6 +1,7 @@
 package com.freshvotes.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,5 +11,10 @@ public class DashBoardController {
     @RequestMapping(value = "/",  method = RequestMethod.GET)
     public String rootView(){
         return "index";
+    }
+
+    @GetMapping(value = "/dashboard")
+    public String dashboardView() {
+        return "dashboard";
     }
 }
